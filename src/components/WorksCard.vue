cc2702ca6d1682c846654be6eb35256d577effc1
tkm <sumikawa.web@gmail.com>
2020-06-10 21:30:09 +0900

    create card

---
+++ src/components/WorksCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card">
+    <img class="card__img" src="../assets/logo.png">
+    <div class="card__txt">
+      <h3 class="card__ttl">作品名</h3>
+      <p class="card__date">2020年10月11日</p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      cardNum: 10
+    }
+  },
+  mounted() {
+    this.$emit('card-num', this.cardNum);
+  }
+}
+</script>
+
+
+<style scoped>
+.card {
+  width: 300px;
+  margin: 10px auto;
+  background-color: #ddd;
+  border: 2px solid #222;
+  border-radius: 7px;
+  transition: box-shadow 0.45s cubic-bezier(0.25, 1, 0.5, 1) 0s;
+}
+.card:hover {
+  box-shadow: 0 0 20px #222;
+}
+.card__img {
+  display: block;
+  width: 100%;
+  background-color: orange;
+}
+.card__txt {
+  padding: 0 10px;
+}
+.card__ttl {
+  font-size: 24px;
+  margin: 10px 0 20px;
+}
+.card__date {
+  padding-bottom: 15px;
+  font-size: 14px;
+  text-align: right;
+}
+</style>
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,12 +1,44 @@
 <template>
-  <div>
-    works
+  <div class="works">
+    <h2 class="works__ttl">Works</h2>
+    <ul class="works-list">
+      <li class="works-list__item" v-for="n in cardNum" :key="n">
+        <WorksCard @card-num="countCard($event)"></WorksCard>
+      </li>
+    </ul>
   </div>
 </template>
 
 
 <script>
+import WorksCard from '../components/WorksCard.vue';
 export default {
-
+  data() {
+    return {
+      cardNum: '0',
+    }
+  },
+  methods: {
+    countCard(cardNum) {
+      this.cardNum = cardNum;
+    }
+  },
+  components: {
+    WorksCard
+  }
 }
 </script>
+
+<style scoped>
+.works {
+
+}
+.works__ttl {
+  font-size: 32px;
+}
+.works-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+</style>
